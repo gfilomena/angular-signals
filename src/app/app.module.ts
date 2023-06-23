feb11528c1affe25187f3061433fa913fe88eb49
Giuseppe Filomena <salsx@hotmail.it>
2023-06-23 18:37:12 +0200

    shop component with signal added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShopComponent } from './shop/shop.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShopComponent
   ],
   imports: [
     BrowserModule,
+    CommonModule,FormsModule,
     AppRoutingModule
   ],
   providers: [],
